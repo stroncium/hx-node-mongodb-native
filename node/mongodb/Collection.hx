a92ab3cb6932345b61fd6c8ea2ecb7b352741387
Yanis Benson <yanis.benson@gmail.com>
2013-02-04 20:01:47 +0400

    find updated

--- node/mongodb/Collection.hx
+++ node/mongodb/Collection.hx
@@ -8,7 +8,7 @@ private typedef CB1<T> = Error->T->Void;
 
 extern class Collection<T>{
   function new(db:Db, name:String):Void;
-  public function find(query:D, ?fields:Array<String>, options:Dynamic):Cursor<T>;
+  public function find(query:D, ?fields:Array<String>, ?options:Dynamic):Cursor<T>;
   public function insert(docs:D, options:D, cb:CB):Void;
   public function update(query:D, upd:D, cb:CB):Void;
   public function aggregate(queries:Array<Dynamic>, cb:CB1<Dynamic>):Void;
